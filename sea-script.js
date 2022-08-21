ce53b3f04485295d922a286433e044b58cb67ded
Nastya <artem.matviienko23@gmail.com>
2022-08-21 15:21:56 +0300

    function to receive a random ship

--- sea-script.js
+++ sea-script.js
@@ -1,84 +1,25 @@
-// function createBattleStatus() {
-//     const string = 'XO. ';
-//     const randomStr = Math.random();
-//     if (randomStr < 0.1) { return string[0];}
-//     if (randomStr < 0.3) {return string[1];}
-//     if (randomStr < 0.6) {return string[2];}
-//     return string[3];
-// }
-//
-// function getBattleStatus() {
-//         return createBattleStatus();
-//     }
-//
-
+const resultArray = [];
 function createArray(numberOfLines, numberOfColumns) {
-    const resultArray = [];
     for (let i = 0; i < numberOfLines; i++) {
         resultArray[i] = [];
         for (let j = 0; j < numberOfColumns; j++) {
-            const value = Math.floor(Math.random()*2);
+            const value = Math.floor(Math.random()*3);
             resultArray[i].push(value);
         }
     }
     return resultArray;
 }
-// console.log(createArray(10, 10));
 
-// function showBattleField(field) {
-//     const arrayTable = document.getElementById('table');
-//     for (let i = 0; i < 10; i++) {
-//         const tr = document.createElement('tr');
-//         for (let j = 0; j < 10; j++) {
-//             const td = document.createElement('td');
-//             td.innerHTML = field[i][j];
-//             if (field[i][j] === 0) {
-//                 td.className = 'grey';
-//                 // document.write(td.className);
-//                 // td.innerHTML = td.className;
-//             }
-//             tr.appendChild(td);
-//         }
-//         arrayTable.appendChild(tr);
-//     }
-//     return arrayTable;
-// }
-//
-// showBattleField(createArray(10, 10));
-
-// function showBattleField(field) {
-//     const arrayTable = document.getElementById('table');
-//     field.forEach(function(row) {
-//         const tr = document.createElement('tr');
-//         field.forEach(function(cell) {
-//             const td = document.createElement('td');
-//             td.innerHTML = cell;
-//             if (cell === 0) {
-//                 td.className = 'grey';
-//                 document.write(td.className);
-//                 // td.innerHTML = td.className;
-//             }
-//             tr.appendChild(td);
-//         })
-//         arrayTable.appendChild(tr);
-//     })
-//     return arrayTable;
-// }
-//
-// showBattleField(createArray(10, 10));
 
 showBattleField = battleField => {
     const arrayTable = document.getElementById('table');
     battleField.forEach((row) => {
         const tr = document.createElement('tr');
-        // let cellF = function(cell) ;
         row.forEach((cell) => {
             const td = document.createElement('td');
             td.innerHTML = cell;
             if (cell === 0) {
                 td.className = 'grey';
-                // document.write(td.className);
-                // td.innerHTML = td.className;
             }
             tr.appendChild(td);
         })
@@ -86,52 +27,22 @@ showBattleField = battleField => {
     })
     return arrayTable;
 }
-
 showBattleField(createArray(10, 10));
 
 let operations ={
     creating: createArray(10, 10),
     showing: showBattleField(createArray(10, 10)),
+    ship: getRandomShip(),
 }
-console.log(operations.creating);
+console.log(operations.ship);
+
+ function getRandomShip() {
+     const randomRow = Math.floor(Math.random()*10);
+     const randomCell = Math.floor(Math.random()*10);
+     resultArray[randomRow][randomCell] = "***";
+     const arrayTable = document.getElementById('table');
+     arrayTable.innerHTML="";
+     showBattleField(resultArray);
+ }
 
 
-// function showTable(lineNumber, cellNumber, shipMarkup) {
-//     const tableLetter = document.getElementById('tableLetter');
-//     for (let i = 0; i < 10; i++) {
-//         const tr = document.createElement('tr');
-//         for (let j = 0; j < 10; j++) {
-//             const td = document.createElement('td');
-//             // if (document.getElementById('ship').value === 1) {
-//                 if (i === lineNumber && j === cellNumber) {td.innerHTML = shipMarkup;}
-//                 else {td.innerHTML = ' ';}
-//             tr.appendChild(td);
-//         }
-//         tableLetter.appendChild(tr);
-//     }
-// }
-// showTable(0, 3, '***');
-
-
-
-function fib(n) {
-    if ( n < -1 || n === -1) {return fib(n+2) - fib(n+1);}
-     if (n === 2) {return 1;}
-    return fib(n-1) + fib(n-2);
-}
-alert(fib(-2));
-
-// function sumTo(num) {
-//     if (num === 0 || num === 1) { return num;}
-//     return num + sumTo(num - 1);
-// }
-// alert(sumTo(-2));
-
-// function sumNumber(num) {
-//     let result = 0;
-//     for (let i = 0; i < num + 1; i++) {
-//        result += i;
-//     }
-//     return result;
-// }
-// alert(sumNumber(-2));
